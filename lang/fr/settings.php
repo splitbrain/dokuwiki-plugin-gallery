ab77ce9a4e03d3fa78a5b33ea466f3338e4b0c6d
manu <manu@seedfloyd.fr>
2007-12-10 20:15:21 +0100

    French translation
    darcs-hash:20071210191521-e367c-e7abd5889a51d3c1ed8ac06ea22a76562b29929c.gz

---
+++ lang/fr/settings.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * French language file for gallery plugin
+ *
+ * @author Dmitry Baikov <dsbaikov@gmail.com>
+ * @french translation Emmanuel Dupin <seedfloyd@gmail.com>
+ */
+
+$lang['thumbnail_width']  = 'Largeur des miniatures';
+$lang['thumbnail_height'] = 'Hauteur des miniatures';
+$lang['image_width']      = 'Largeur des images';
+$lang['image_height']     = 'Hauteur des images';
+$lang['cols']             = 'Nombre d\'images par ligne';
+$lang['direct']           = 'Lien direct';
+$lang['lightbox']         = 'Utiliser Lightbox (force l\'option Lien direct)';
+$lang['showname']         = 'Montrer le nom du fichier image';
+$lang['reverse']          = 'Inverser l\'ordre de tri';
