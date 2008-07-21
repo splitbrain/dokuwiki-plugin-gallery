f3ab21c50d7a849e268716f8012532b6b72c39d5
Tomas Drbohlav <tom@drb.cz>
2008-07-21 21:40:57 +0200

    Czech translation
    darcs-hash:20080721194057-1033f-9bb805f717b968827c7484c9818230e82ed6a594.gz

---
+++ lang/cs/settings.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Czech language file for gallery plugin
+ *
+ * @author Tomáš Drbohlav <tom@drb.cz>
+ */
+
+$lang['thumbnail_width']  = 'Šířka náhledu';
+$lang['thumbnail_height'] = 'Výška náhledu';
+$lang['image_width']      = 'Šířka obrázku';
+$lang['image_height']     = 'Výška obrázku';
+$lang['cols']             = 'Obrázků na řádku';
+$lang['direct']           = 'Přímé odkazy';
+$lang['lightbox']         = 'Použít Lightbox (implikuje přímé odkazy)';
+$lang['showname']         = 'Zobrazovat názvy souborů';
+$lang['reverse']          = 'Třídit opačně';
