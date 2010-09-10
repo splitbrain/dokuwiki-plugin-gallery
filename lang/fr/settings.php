1b51bb266747bf658c74a1f1e939f464fbaecdb0
Emmanuel Dupin <seedfloyd@gmail.com>
2010-09-10 19:57:42 +0200

    French language update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -3,7 +3,7 @@
  * French language file for gallery plugin
  *
  * @author Dmitry Baikov <dsbaikov@gmail.com>
- * @french translation Emmanuel Dupin <seedfloyd@gmail.com>
+ * @author Emmanuel Dupin <seedfloyd@gmail.com>
  */
 
 $lang['thumbnail_width']  = 'Largeur des miniatures';
@@ -11,3 +11,11 @@ $lang['thumbnail_height'] = 'Hauteur des miniatures';
 $lang['image_width']      = 'Largeur des images';
 $lang['image_height']     = 'Hauteur des images';
 $lang['cols']             = 'Nombre d\'images par ligne';
+
+$lang['sort']         = 'Ordre de tri des images';
+$lang['sort_o_file']  = 'trier par nom du fichier';
+$lang['sort_o_mod']   = 'trier par date de modification du fichier';
+$lang['sort_o_date']  = 'trier par date EXIF';
+$lang['sort_o_title'] = 'trier par titre EXIF';
+
+$lang['options'] = 'Options par défaut supplémentaires';
