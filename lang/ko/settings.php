971a85d772c27d71502d79712eb8714ba254e16d
Dryoo <dryoo@live.com>
2013-07-28 02:04:37 +0200

    Korean translation added

---
+++ lang/ko/settings.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Korean language file for gallery plugin
+ *
+ * @author Dmitry Baikov <dsbaikov@gmail.com>
+ * @author Andreas Gohr <andi@splitbrain.org>
+ * @author SC Yoo <dryoo@live.com>
+ */
+
+$lang['thumbnail_width']  = '썸네일 그림 너비';
+$lang['thumbnail_height'] = '썸네일 그림 높이';
+$lang['image_width']      = '그림 너비';
+$lang['image_height']     = '그림 높이';
+$lang['cols']             = '줄당 그림 수';
+
+$lang['sort']         = '갤러리 그림 정렬 방법';
+$lang['sort_o_file']  = '파일이름 순서';
+$lang['sort_o_mod']   = '파일날짜 순서';
+$lang['sort_o_date']  = 'EXIF 날짜 순서';
+$lang['sort_o_title'] = 'EXIF 제목 순서';
+
+$lang['options'] = '부가적인 갤러리 기본 설정';
+
