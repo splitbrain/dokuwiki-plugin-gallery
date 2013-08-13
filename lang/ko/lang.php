d9e88faef5e236f61f55748de56c1e9ca49f8177
Myeongjin <aranet100@gmail.com>
2013-08-13 14:35:05 +0200

    translation update

--- lang/ko/lang.php
+++ lang/ko/lang.php
@@ -1,4 +1,8 @@
 <?php
 
-$lang['pages'] = '갤러리 페이지:';
-$lang['js']['addgal'] = '갤러리로 이름공간 추가';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ */
+$lang['pages']                 = '갤러리 페이지:';
+$lang['js']['addgal']          = '갤러리로 이름공간 추가';
