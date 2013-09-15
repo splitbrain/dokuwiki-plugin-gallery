2716d42006d95317ffb97d98bcee518702883e83
schplurtz <Schplurtz@laposte.net>
2013-09-15 16:35:05 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -1,21 +1,19 @@
 <?php
+
 /**
- * French language file for gallery plugin
- *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
  * @author Dmitry Baikov <dsbaikov@gmail.com>
  * @author Emmanuel Dupin <seedfloyd@gmail.com>
  */
-
-$lang['thumbnail_width']  = 'Largeur des miniatures';
-$lang['thumbnail_height'] = 'Hauteur des miniatures';
-$lang['image_width']      = 'Largeur des images';
-$lang['image_height']     = 'Hauteur des images';
-$lang['cols']             = 'Nombre d\'images par ligne';
-
-$lang['sort']         = 'Ordre de tri des images';
-$lang['sort_o_file']  = 'trier par nom du fichier';
-$lang['sort_o_mod']   = 'trier par date de modification du fichier';
-$lang['sort_o_date']  = 'trier par date EXIF';
-$lang['sort_o_title'] = 'trier par titre EXIF';
-
-$lang['options'] = 'Options par défaut supplémentaires';
+$lang['thumbnail_width']       = 'Largeur des miniatures';
+$lang['thumbnail_height']      = 'Hauteur des miniatures';
+$lang['image_width']           = 'Largeur des images';
+$lang['image_height']          = 'Hauteur des images';
+$lang['cols']                  = 'Nombre d\'images par ligne';
+$lang['sort']                  = 'Ordre de tri des images';
+$lang['sort_o_file']           = 'trier par nom du fichier';
+$lang['sort_o_mod']            = 'trier par date de modification du fichier';
+$lang['sort_o_date']           = 'trier par date EXIF';
+$lang['sort_o_title']          = 'trier par titre EXIF';
+$lang['options']               = 'Options par défaut supplémentaires';
