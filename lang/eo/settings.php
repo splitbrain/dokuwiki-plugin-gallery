a56bc56539605fe80d074071815b51ea950ad914
Robert Bogenschneider <bogi@uea.org>
2013-10-29 21:30:05 +0100

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <bogi@uea.org>
+ */
+$lang['thumbnail_width']       = 'Bildet-larĝeco';
+$lang['thumbnail_height']      = 'Bildet-alteco';
+$lang['image_width']           = 'Bildlarĝeco';
+$lang['image_height']          = 'Bildalteco';
+$lang['cols']                  = 'Bildoj po vico';
+$lang['sort']                  = 'Kiel ordigi la galeribildojn';
+$lang['sort_o_file']           = 'ordigi per dosiernomo';
+$lang['sort_o_mod']            = 'ordigi per dosierdato';
+$lang['sort_o_date']           = 'ordigi per EXIF-dato';
+$lang['sort_o_title']          = 'ordigi per EXIF-titolo';
+$lang['options']               = 'Aldonaj galeriaj standardaj agordoj';
