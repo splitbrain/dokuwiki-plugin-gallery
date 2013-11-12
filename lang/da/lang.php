c3f3f1d6abf8c2e900c5ef2656dfd4822fc0e417
soer9648 <soer9648@eucl.dk>
2013-11-12 20:53:58 +0100

    translation update

---
+++ lang/da/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author soer9648 <soer9648@eucl.dk>
+ */
+$lang['pages']                 = 'Gallerisider:';
+$lang['js']['addgal']          = 'Tilføj navnerum som galleri';
---
+++ lang/da/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author soer9648 <soer9648@eucl.dk>
+ */
+$lang['thumbnail_width']       = 'Miniature bredde';
+$lang['thumbnail_height']      = 'Miniature højde';
+$lang['image_width']           = 'Billede bredde';
+$lang['image_height']          = 'Billede højde';
+$lang['cols']                  = 'Billeder per række';
+$lang['sort']                  = 'Hvordan skal galleribilleder sorteres';
+$lang['sort_o_file']           = 'sortér efter filnavn';
+$lang['sort_o_mod']            = 'sortér efter fildato';
+$lang['sort_o_date']           = 'sortér efter EXIF-dato';
+$lang['sort_o_title']          = 'sortér efter EXIF-titel';
+$lang['options']               = 'Yderligere galleri-standardindstillinger';
