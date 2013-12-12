ecbbc87e23c7605450f9d4f2c0c7d46f2f6143cc
bruno <bruno@ninesys.fr>
2013-12-12 23:05:05 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author bruno <bruno@ninesys.fr>
+ */
+$lang['pages']                 = 'Galeries :';
+$lang['js']['addgal']          = 'Ajouter un nom à la galerie';
