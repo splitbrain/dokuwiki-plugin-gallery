d7b9c98de95d1c7b45f31a0a6c101a4991d7048f
Nicolas Friedli <nicolas@theologique.ch>
2013-12-17 14:30:18 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Nicolas Friedli <nicolas@theologique.ch>
+ */
+$lang['pages']                 = 'Pages de galerie';
+$lang['js']['addgal']          = 'Ajouter un espace de nom en tant que galerie';
