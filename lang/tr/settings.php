378987f0b9d5f30e1364bdbff8c1da3656ff6fa7
Shiliew <siskyblue@hotmail.com>
2014-01-17 04:55:14 +0100

    translation update

---
+++ lang/tr/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Shiliew <siskyblue@hotmail.com>
+ */
+$lang['thumbnail_width']       = 'Küçük resim genişligi';
+$lang['thumbnail_height']      = 'Küçük resim yüksekligi';
+$lang['image_width']           = 'Resim Genişligi';
+$lang['image_height']          = 'Resim yüksekligi';
+$lang['cols']                  = 'Görüntü başına satır';
+$lang['sort']                  = 'Resimlerin nasıl Sıralanacagı';
+$lang['sort_o_file']           = 'Dosya adına göre sırala';
+$lang['sort_o_mod']            = 'Dosya tarihine göre sıralama';
+$lang['sort_o_date']           = 'Sırlama çıkış tarihi';
+$lang['sort_o_title']          = 'Sıralama çıkış başlıgı';
+$lang['options']               = 'Ek galeri varsayılan seçenekler';
