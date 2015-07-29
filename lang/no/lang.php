69754892b25582d7a344d0c62020bba5f8e8f7b1
Egil Hansen <egil.hansen@elite.as>
2015-07-29 15:05:35 +0200

    translation update

---
+++ lang/no/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Egil Hansen <egil.hansen@elite.as>
+ */
+$lang['pages']                 = 'Gallerisider:';
+$lang['js']['addgal']          = 'Legg til navnerom som galleri';
---
+++ lang/no/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Egil Hansen <egil.hansen@elite.as>
+ */
+$lang['thumbnail_width']       = 'Miniatyrbilde bredde';
+$lang['thumbnail_height']      = 'Miniatyrbilde høyde';
+$lang['image_width']           = 'Bilde bredde';
+$lang['image_height']          = 'Bilde høyde';
+$lang['cols']                  = 'Bilder pr. rad';
+$lang['sort']                  = 'Hvordan galleribildene sorteres ';
+$lang['sort_o_file']           = 'etter filnavn';
+$lang['sort_o_mod']            = 'etter dato';
+$lang['sort_o_date']           = 'etter EXIF-dato';
+$lang['sort_o_title']          = 'etter EXIF-tittel';
+$lang['options']               = 'Andre standardinstillinger for gallerier';
