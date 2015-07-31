5fb223824675be51dfbd67ff8b93d8c851a1e8e0
Torq <torkar-b@online.no>
2015-07-31 12:30:05 +0200

    translation update

---
+++ lang/no/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Torq <torkar-b@online.no>
+ */
+$lang['pages']                 = 'Gallerisider:';
+$lang['js']['addgal']          = 'Legg navnerom til som galleri';
---
+++ lang/no/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Torq <torkar-b@online.no>
+ */
+$lang['thumbnail_width']       = 'Vidden på miniatyrbilder';
+$lang['thumbnail_height']      = 'Høyden på miniatyrbilder';
+$lang['image_width']           = 'Bildevidde';
+$lang['image_height']          = 'Bildebredde';
+$lang['cols']                  = 'Bilder pr. rad';
+$lang['sort']                  = 'Hvordan sortere galleri-bilder';
+$lang['sort_o_file']           = 'sorter etter filnavn';
+$lang['sort_o_mod']            = 'sorter etter dato';
+$lang['sort_o_date']           = 'Sorter etter EXIF-dato';
+$lang['sort_o_title']          = 'Sorter etter EXIF-tittel';
+$lang['options']               = 'Ytterligere standardalternativer for galleri';
