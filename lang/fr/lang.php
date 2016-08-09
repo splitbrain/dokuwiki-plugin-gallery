f35eb0813a40b597382278eeab63f203a644cc81
kandin <0699419485km@gmail.com>
2016-08-09 03:45:21 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Christian "Na_kai" Sueur <sueur.christian@gmail.com>
  * @author NicolasFriedli <nicolas@theologique.ch>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
