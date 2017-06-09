4d096f194359b92d1ee2cb24d624115ca2a77062
Antonio <antonioippolito@gmail.com>
2017-06-09 21:30:31 +0200

    translation update

--- lang/it/settings.php
+++ lang/it/settings.php
@@ -1,18 +1,19 @@
 <?php
+
 /**
- * Italian language file for gallery plugin
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
  * @author Dmitry Baikov <dsbaikov@gmail.com>
  * @author Diego Pierotto <ita.translations@tiscali.it>
+ * @author Antonio <antonioippolito@gmail.com>
  */
-
-$lang['thumbnail_width']  = 'Larghezza immagine anteprima';
-$lang['thumbnail_height'] = 'Altezza immagine anteprima';
-$lang['image_width']      = 'Larghezza immagine';
-$lang['image_height']     = 'Altezza immagine';
-$lang['cols']             = 'Immagini per riga';
-$lang['direct']           = 'Collegamento direct';
-$lang['lightbox']         = 'Utilizza Lightbox (implica il collegamento diretto)';
-$lang['showname']         = 'Mostra nome file immagine';
-$lang['reverse']          = 'Inverti ordine';
-$lang['js_ok']  = 'Allow javascript urls';
+$lang['thumbnail_width']       = 'Larghezza immagine anteprima';
+$lang['thumbnail_height']      = 'Altezza immagine anteprima';
+$lang['image_width']           = 'Larghezza immagine';
+$lang['image_height']          = 'Altezza immagine';
+$lang['cols']                  = 'Immagini per riga';
+$lang['sort']                  = 'Come ordinare le immagini della galleria';
+$lang['sort_o_file']           = 'ordina per nome del file';
+$lang['sort_o_mod']            = 'ordina per data del file';
+$lang['sort_o_date']           = 'ordina per data dell\'EXIF';
+$lang['sort_o_title']          = 'ordina per titolo dell\'EXIF';
