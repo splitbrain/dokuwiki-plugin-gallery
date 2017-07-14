bf15811802acd8ab8dc8c8406eacf48af67f59bb
Tor Härnqvist <tor@harnqvist.se>
2017-07-14 16:45:20 +0200

    translation update

---
+++ lang/sv/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Tor Härnqvist <tor@harnqvist.se>
+ */
+$lang['thumbnail_width']       = 'Bredd på miniatyrbild';
+$lang['thumbnail_height']      = 'Höjd på miniatyrbild';
+$lang['image_width']           = 'Bildbredd';
+$lang['image_height']          = 'Bildhöjd';
+$lang['cols']                  = 'Bilder per rad';
+$lang['sort']                  = 'Sortering av bilderna';
+$lang['sort_o_file']           = 'Sortera efter filtitel';
+$lang['sort_o_mod']            = 'Sortera efter fildatum';
+$lang['sort_o_date']           = 'Sortera efter EXIF-datum';
+$lang['sort_o_title']          = 'Sortera efter EXIF-titel';
+$lang['options']               = 'Ytterligare standardval för bildarkivet';
