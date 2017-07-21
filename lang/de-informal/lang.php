c213021918c09e248b8b25cd8ef96e9ff5dba645
Felix Müller-Donath <j.felix@mueller-donath.de>
2017-07-21 15:20:29 +0200

    translation update

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Felix Müller-Donath <j.felix@mueller-donath.de>
+ */
+$lang['pages']                 = 'Galerie-Seiten:';
+$lang['js']['addgal']          = 'Namensraum als Galerie hinzufügen';
