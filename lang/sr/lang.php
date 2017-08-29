de54d3fbb6b30361fb98a46600b2793cf4703ef2
Марко М. Костић <marko.m.kostic@gmail.com>
2017-08-29 18:46:39 +0200

    translation update

---
+++ lang/sr/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Марко М. Костић <marko.m.kostic@gmail.com>
+ */
+$lang['pages']                 = 'Странице галерије:';
+$lang['js']['addgal']          = 'Додај именски простор као галерију';
---
+++ lang/sr/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Марко М. Костић <marko.m.kostic@gmail.com>
+ */
+$lang['thumbnail_width']       = 'Ширина сличице';
+$lang['thumbnail_height']      = 'Висина сличице';
+$lang['image_width']           = 'Ширина слике';
+$lang['image_height']          = 'Висина слике';
+$lang['cols']                  = 'Слика по реду';
+$lang['sort']                  = 'Како треба ређати слике у галерији';
+$lang['sort_o_file']           = 'ређај по имену датотеке';
+$lang['sort_o_mod']            = 'ређају по датуму настанка';
+$lang['sort_o_date']           = 'ређај по EXIF датуму';
+$lang['sort_o_title']          = 'ређај по EXIF наслову';
+$lang['options']               = 'Додатне подразумеване опције галерије';
