f91080723464e1bd772765486004e3cf1f9bfdd1
powerslave <powerslave@bopx.ch>
2017-10-11 15:10:15 +0200

    stop event bubbling on close action
    https://github.com/splitbrain/dokuwiki-plugin-gallery/issues/100

--- swipebox/js/jquery.swipebox.js
+++ swipebox/js/jquery.swipebox.js
@@ -575,7 +575,9 @@
 					} );
 				}
 
-				$( '#swipebox-close' ).bind( action, function() {
+				$( '#swipebox-close' ).bind( action, function(event) {
+					event.preventDefault();
+					event.stopPropagation();
 					$this.closeSlide();
 				} );
 			},
