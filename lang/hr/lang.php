be864343c63d60d6318587a97ac5788b4a4e742b
Davor Turkalj <turki.bsc@gmail.com>
2018-01-22 18:40:05 +0100

    translation update

---
+++ lang/hr/lang.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Davor Turkalj <turki.bsc@gmail.com>
+ */
+$lang['pages']                 = 'Stranice galerije:';
+$lang['js']['addgal']          = 'Dodaj imenski prostor kao galeriju';
---
+++ lang/hr/settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Davor Turkalj <turki.bsc@gmail.com>
+ */
+$lang['thumbnail_width']       = 'Širina slikovnih minijatura';
+$lang['thumbnail_height']      = 'Visina slikovnih minijatura';
+$lang['image_width']           = 'Širina slike';
+$lang['image_height']          = 'Visina slike';
+$lang['cols']                  = 'Slika u retku';
+$lang['sort']                  = 'Kako poredati slike u galeriji';
+$lang['sort_o_file']           = 'poredaj po imenu';
+$lang['sort_o_mod']            = 'poredaj po datumu datoteke';
+$lang['sort_o_date']           = 'poredaj po EXIF datumu';
+$lang['sort_o_title']          = 'poredaj po EXIF naslovu';
+$lang['options']               = 'Dodatne podrazumijevane opcije galerije';
