ec760aa99ef10f5d7280d1eec58da8d0e470b505
Giovanni <giovanni.antonelli@gmail.com>
2018-03-21 14:50:33 +0100

    translation update

--- lang/it/settings.php
+++ lang/it/settings.php
@@ -3,6 +3,7 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Giovanni <giovanni.antonelli@gmail.com>
  * @author Dmitry Baikov <dsbaikov@gmail.com>
  * @author Diego Pierotto <ita.translations@tiscali.it>
  * @author Antonio <antonioippolito@gmail.com>
@@ -17,3 +18,4 @@ $lang['sort_o_file']           = 'ordina per nome del file';
 $lang['sort_o_mod']            = 'ordina per data del file';
 $lang['sort_o_date']           = 'ordina per data dell\'EXIF';
 $lang['sort_o_title']          = 'ordina per titolo dell\'EXIF';
+$lang['options']               = 'Opzioni predefinite aggiuntive galleria';
