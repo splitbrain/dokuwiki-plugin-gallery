97d948629a6aad5adea07ba87e8a03e9b73f030e
Christoph Ziehr <christoph@christoph-ziehr.de>
2019-01-05 22:47:40 +0100

    Own localization string for "nothing found" (#121)

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -4,6 +4,8 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Andreas Gohr <andi@splitbrain.org>
+ * @author Christoph Ziehr <info@einsatzleiterwiki.de>
  */
 $lang['pages']                 = 'Galerie-Seiten:';
 $lang['js']['addgal']          = 'Namensraum als Galerie hinzufügen';
+$lang['nothingfound']          = 'Keine Bilder gefunden.';
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -2,3 +2,4 @@
 
 $lang['pages'] = 'Gallery Pages:';
 $lang['js']['addgal'] = 'Add namespace as gallery';
+$lang['nothingfound'] = 'No images found.';
