9b3e422164635a010f889cf60b62432139c30a99
Christoph Ziehr <christoph@christoph-ziehr.de>
2019-01-05 22:39:24 +0100

    Update lang.php

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -2,4 +2,4 @@
 
 $lang['pages'] = 'Gallery Pages:';
 $lang['js']['addgal'] = 'Add namespace as gallery';
-$lang['nothingfound'] = 'Keine Bilder gefunden.';
+$lang['nothingfound'] = 'No images found.';
