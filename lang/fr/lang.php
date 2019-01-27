fa1fc988ebd51b6bc2e85c7d940ae3d5f611d093
Bruno Généré <bruno.genere@encom1.fr>
2019-01-27 12:35:27 +0100

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,10 +2,12 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Bruno Généré <bruno.genere@encom1.fr>
  * @author Christian "Na_kai" Sueur <sueur.christian@gmail.com>
  * @author NicolasFriedli <nicolas@theologique.ch>
  * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
-$lang['pages']                 = 'Pages galerie:';
+$lang['pages']                 = 'Pages de la galerie :';
 $lang['js']['addgal']          = 'Utiliser cette catégorie comme galerie';
+$lang['nothingfound']          = 'Pas d\'image trouvée';
