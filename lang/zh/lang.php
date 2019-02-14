b854df8e7cf08840310840ade0c83356027da325
RainSlide <RainSlide@outlook.com>
2019-02-14 17:15:33 +0100

    translation update

--- lang/zh/lang.php
+++ lang/zh/lang.php
@@ -2,8 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author RainSlide <RainSlide@outlook.com>
  * @author oott123 <ip.192.168.1.1@qq.com>
  */
-$lang['pages']                 = '相册页面';
+$lang['pages']                 = '相册页面：';
 $lang['js']['addgal']          = '将命名空间添加为相册';
+$lang['nothingfound']          = '找不到图片。';
