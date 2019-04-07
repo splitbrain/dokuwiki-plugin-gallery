a2b03cbb0a860fbe1025300f7871c02e4e4304a8
Yllelder <yllelder@gmail.com>
2019-04-07 08:25:37 +0200

    translation update

--- lang/es/lang.php
+++ lang/es/lang.php
@@ -2,9 +2,11 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Yllelder <yllelder@gmail.com>
  * @author Joel Cantoral <cantoral.joel@gmail.com>
  * @author Domingo Redal <docxml@gmail.com>
  */
-$lang['pages']                 = 'Páginas de Galería';
-$lang['js']['addgal']          = 'Añadir espacio de nombres de galería';
+$lang['pages']                 = 'Páginas de la galería:';
+$lang['js']['addgal']          = 'Añadir espacio de nombres como galería';
+$lang['nothingfound']          = 'No se han encontrado imágenes.';
