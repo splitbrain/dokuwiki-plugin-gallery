adcf3a1434bf5fe8a98faafecbe65176f6d200d3
Roberto Bellingeri <bellingeri@netguru.it>
2019-04-17 15:20:13 +0200

    translation update

--- lang/it/lang.php
+++ lang/it/lang.php
@@ -3,7 +3,9 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Roberto Bellingeri <bellingeri@netguru.it>
  * @author Giovanni <giovanni.antonelli@gmail.com>
  */
 $lang['pages']                 = 'Pagine Galleria';
 $lang['js']['addgal']          = 'Aggiungi nome alla galleria';
+$lang['nothingfound']          = 'Nessuna immagine trovata.';
