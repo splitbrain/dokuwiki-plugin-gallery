dc05f8e6eef75fcc039060e701571b8636b82479
android405 <android40561@ya.ru>
2019-04-23 18:55:05 +0200

    translation update

--- lang/ru/lang.php
+++ lang/ru/lang.php
@@ -2,8 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author android405 <android40561@ya.ru>
  * @author Aleksandr Selivanov <alexgearbox@yandex.ru>
  */
 $lang['pages']                 = 'Страницы галереи:';
 $lang['js']['addgal']          = 'Добавить пространство имён как галерею';
+$lang['nothingfound']          = 'Изображения не найдены';
