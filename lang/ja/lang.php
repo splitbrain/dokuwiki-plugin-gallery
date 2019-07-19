ad3739110686180a81b938f6deab465fd7b06a73
king <b4688756@urhen.com>
2019-07-19 09:55:21 +0200

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -2,8 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author king <b4688756@urhen.com>
  * @author Nobuyuki Fukuyori <self@spumoni.org>
  */
 $lang['pages']                 = 'ギャラリー・ページ';
 $lang['js']['addgal']          = '名前空間の追加';
+$lang['nothingfound']          = '画像が見つかりません';
