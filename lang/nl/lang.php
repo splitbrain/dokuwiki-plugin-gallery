f204bc4c48404f7f8815b8aacb22e93401370679
Gerrit Uitslag <klapinklapin@gmail.com>
2019-08-12 20:30:07 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -2,8 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Gerrit Uitslag <klapinklapin@gmail.com>
  * @author Hugo Smet <hugo.smet@outlook.com>
  */
 $lang['pages']                 = 'Beeldreeks pagina\'s:';
 $lang['js']['addgal']          = 'Voeg naamruimte toe als beeldreeks';
+$lang['nothingfound']          = 'Geen afbeeldingen gevonden.';
