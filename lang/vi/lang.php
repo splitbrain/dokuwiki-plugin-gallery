636132a39bb0eedff78d962c9274b0db0737138c
Thien Hau <thienhausoftware@gmail.com>
2020-05-07 04:35:36 +0200

    translation update

---
+++ lang/vi/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Thien Hau <thienhausoftware@gmail.com>
+ */
+$lang['pages']                 = 'Trang thư viên';
+$lang['js']['addgal']          = 'Thêm không gian tên làm thư viện';
+$lang['nothingfound']          = 'Không tìm thấy hình ảnh.';
