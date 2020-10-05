9cfdd90d7847f252586ce055038e356e2c9753be
qezwan <qezwan@gmail.com>
2020-10-05 23:45:52 +0200

    translation update

---
+++ lang/ckb/lang.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author qezwan <qezwan@gmail.com>
+ */
+$lang['pages']                 = 'گەلەری لاپەڕەکان:';
+$lang['js']['addgal']          = 'زیادکردنی بۆشایی ناو وەک گالەری';
+$lang['nothingfound']          = 'هیچ وێنەیەک نەدۆزرایەوە.';
