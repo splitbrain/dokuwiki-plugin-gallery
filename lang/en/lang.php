03a6308533b295dba44ae123416053017b54e8be
crystal green <victoriaguyton2014@gmail.com>
2022-03-15 04:50:21 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,5 +1,9 @@
 <?php
 
-$lang['pages'] = 'Gallery Pages:';
-$lang['js']['addgal'] = 'Add namespace as gallery';
-$lang['nothingfound'] = 'No images found.';
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+$lang['pages']                 = 'Gallery Pages:';
+$lang['js']['addgal']          = 'Add namespace as gallery';
+$lang['nothingfound']          = 'No images found.';
