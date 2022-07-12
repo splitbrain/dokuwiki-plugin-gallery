3f6cf5f40c5ce9377b602c3bfe944851b565a151
Javier Ramos <co2mm.esperanto@gmail.com>
2022-07-12 10:05:30 +0200

    translation update

--- lang/eo/lang.php
+++ lang/eo/lang.php
@@ -2,8 +2,10 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author Javier Ramos <co2mm.esperanto@gmail.com>
  * @author Robert Bogenschneider <bogi@uea.org>
  */
 $lang['pages']                 = 'Galeripaĝoj:';
 $lang['js']['addgal']          = 'Aldoni nomspacon kiel galerio';
+$lang['nothingfound']          = 'Neniu bildo trovita';
