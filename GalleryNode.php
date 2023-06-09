e71049880c7b1bea725ce99b1b037a6fa1c5818c
Andreas Gohr <andi@splitbrain.org>
2023-06-09 20:18:47 +0200

    fix error when prosemirror is not installed

---
+++ GalleryNode.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace dokuwiki\plugin\gallery;
+
+use dokuwiki\plugin\prosemirror\parser\Node;
+
+class GalleryNode extends Node
+{
+
+    protected $parent;
+    protected $data;
+
+    /**
+     * GalleryNode constructor.
+     *
+     * Todo: This constructor will likely by abstracted away into a parent class or something at somepoint.
+     *
+     * @param $data
+     * @param {Node} $parent
+     */
+    public function __construct($data, Node $parent)
+    {
+        $this->parent = &$parent;
+        $this->data = $data;
+    }
+
+    /**
+     * Get the node's representation as DokuWiki Syntax
+     *
+     * @return string
+     */
+    public function toSyntax()
+    {
+        /** @var syntax_plugin_gallery $syntax */
+        $syntax = plugin_load('syntax', 'gallery');
+        $defaults = $syntax->getDataFromParams($syntax->getConf('options'));
+        /** @var action_plugin_gallery_prosemirror $action */
+        $action = plugin_load('action', 'gallery_prosemirror');
+        $defaults = $action->cleanAttributes($defaults);
+        $query = [];
+        $attrs = $this->data['attrs'];
+        if ($attrs['thumbnailsize'] !== $defaults['thumbnailsize']) {
+            $query[] = $attrs['thumbnailsize'];
+        }
+        if ($attrs['imagesize'] !== $defaults['imagesize']) {
+            $query[] = $attrs['imagesize'];
+        }
+
+        $query[] = $this->extractFlagParam('showname', $attrs, $defaults);
+        $query[] = $this->extractFlagParam('showtitle', $attrs, $defaults);
+        $query[] = $this->extractFlagParam('cache', $attrs, $defaults);
+        $query[] = $this->extractFlagParam('crop', $attrs, $defaults);
+        $query[] = $this->extractFlagParam('direct', $attrs, $defaults);
+        $query[] = $this->extractFlagParam('lightbox', $attrs, $defaults);
+        $query[] = $this->extractFlagParam('reverse', $attrs, $defaults);
+        $query[] = $this->extractFlagParam('recursive', $attrs, $defaults);
+        $query = array_filter($query);
+
+        if ((int)$attrs['cols'] !== (int)$defaults['cols']) {
+            $query[] = $attrs['cols'];
+        }
+        if ((int)$attrs['limit'] !== (int)$defaults['limit']) {
+            $query[] = '=' . $attrs['limit'];
+        }
+        if ((int)$attrs['offset'] !== (int)$defaults['offset']) {
+            $query[] = '+' . $attrs['offset'];
+        }
+        if ((int)$attrs['paginate'] !== (int)$defaults['paginate']) {
+            $query[] = '~' . $attrs['paginate'];
+        }
+        if ((int)$attrs['sort'] !== (int)$defaults['sort']) {
+            $query[] = $attrs['sort'];
+        }
+        if ($attrs['filter'] && strpos($attrs['filter'], '*') !== false) {
+            $query[] = $attrs['filter'];
+        }
+        $alignLeft = $attrs['align'] === 'left' ? ' ' : '';
+        $alignRight = $attrs['align'] === 'right' ? ' ' : '';
+        $result = '{{gallery>' . $alignRight . $attrs['namespace'];
+        if (!empty($query)) {
+            $result .= '?' . implode('&', $query);
+        }
+        $result .= $alignLeft . '}}';
+        return $result;
+    }
+
+    /**
+     * Get syntax option-string if the option is different from the default
+     *
+     * @param string $paramName The name of the parameter
+     * @param array $data The node's attributes from the editor
+     * @param array $defaults The default options
+     *
+     * @return null|string Either the string to toggle the option away from the default or null
+     */
+    protected function extractFlagParam($paramName, $data, $defaults)
+    {
+        if ((bool)$data[$paramName] !== (bool)$defaults[$paramName]) {
+            return ($defaults[$paramName] ? 'no' : '') . $paramName;
+        }
+        return null;
+    }
+}
--- action/prosemirror.php
+++ action/prosemirror.php
@@ -1,5 +1,7 @@
 <?php
 
+use dokuwiki\plugin\gallery\GalleryNode;
+use dokuwiki\plugin\prosemirror\parser\RootNode;
 use dokuwiki\plugin\prosemirror\schema\Node;
 
 class action_plugin_gallery_prosemirror extends DokuWiki_Action_Plugin
@@ -13,6 +15,9 @@ class action_plugin_gallery_prosemirror extends DokuWiki_Action_Plugin
      */
     public function register(Doku_Event_Handler $controller)
     {
+        // check if prosemirror is installed
+        if (!class_exists('\dokuwiki\plugin\prosemirror\schema\Node')) return;
+
         $controller->register_hook('DOKUWIKI_STARTED', 'BEFORE', $this, 'writeDefaultsToJSINFO');
         $controller->register_hook('PROSEMIRROR_RENDER_PLUGIN', 'BEFORE', $this, 'renderFromInstructions');
         $controller->register_hook('PROSEMIRROR_PARSE_UNKNOWN', 'BEFORE', $this, 'parseToSyntax');
@@ -24,8 +29,8 @@ class action_plugin_gallery_prosemirror extends DokuWiki_Action_Plugin
      *
      * Triggered by event: DOKUWIKI_STARTED
      *
-     * @param Doku_Event $event  event object
-     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
+     * @param Doku_Event $event event object
+     * @param mixed $param [the parameters passed as fifth argument to register_hook() when this
      *                           handler was registered]
      *
      * @return void
@@ -43,7 +48,9 @@ class action_plugin_gallery_prosemirror extends DokuWiki_Action_Plugin
             $JSINFO['plugins'] = [];
         }
         $JSINFO['plugins']['gallery'] = [
-            'defaults' => array_map(function($default) { return ['default' => $default,];}, $attributes),
+            'defaults' => array_map(function ($default) {
+                return ['default' => $default,];
+            }, $attributes),
         ];
         $JSINFO['plugins']['gallery']['defaults']['namespace'] = ['default' => ''];
     }
@@ -54,8 +61,8 @@ class action_plugin_gallery_prosemirror extends DokuWiki_Action_Plugin
      *
      * Triggered by event: PROSEMIRROR_RENDER_PLUGIN
      *
-     * @param Doku_Event $event  event object
-     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
+     * @param Doku_Event $event event object
+     * @param mixed $param [the parameters passed as fifth argument to register_hook() when this
      *                           handler was registered]
      *
      * @return void
@@ -120,8 +127,8 @@ class action_plugin_gallery_prosemirror extends DokuWiki_Action_Plugin
      *
      * Triggered by event: PROSEMIRROR_PARSE_UNKNOWN
      *
-     * @param Doku_Event $event  event object
-     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
+     * @param Doku_Event $event event object
+     * @param mixed $param [the parameters passed as fifth argument to register_hook() when this
      *                           handler was registered]
      *
      * @return void
@@ -142,13 +149,14 @@ class action_plugin_gallery_prosemirror extends DokuWiki_Action_Plugin
      *
      * Triggered by event: AJAX_CALL_UNKNOQN
      *
-     * @param Doku_Event $event  event object
-     * @param mixed      $param  [the parameters passed as fifth argument to register_hook() when this
+     * @param Doku_Event $event event object
+     * @param mixed $param [the parameters passed as fifth argument to register_hook() when this
      *                           handler was registered]
      *
      * @return void
      */
-    public function renderAttributesToHTML(Doku_Event $event, $param) {
+    public function renderAttributesToHTML(Doku_Event $event, $param)
+    {
         if ($event->data !== 'plugin_gallery_prosemirror') {
             return;
         }
@@ -156,106 +164,10 @@ class action_plugin_gallery_prosemirror extends DokuWiki_Action_Plugin
         $event->stopPropagation();
 
         global $INPUT;
-        $node = new GalleryNode(['attrs' => json_decode($INPUT->str('attrs'), true)], new \dokuwiki\plugin\prosemirror\parser\RootNode([]));
+        $node = new GalleryNode(['attrs' => json_decode($INPUT->str('attrs'), true)], new RootNode([]));
         $syntax = $node->toSyntax();
         $html = p_render('xhtml', p_get_instructions($syntax), $info);
         echo $html;
     }
 }
 
-class GalleryNode extends \dokuwiki\plugin\prosemirror\parser\Node {
-
-    protected $parent;
-    protected $data;
-
-    /**
-     * GalleryNode constructor.
-     *
-     * Todo: This constructor will likely by abstracted away into a parent class or something at somepoint.
-     *
-     * @param $data
-     * @param {Node} $parent
-     */
-    public function __construct($data, \dokuwiki\plugin\prosemirror\parser\Node $parent)
-    {
-        $this->parent = &$parent;
-        $this->data = $data;
-    }
-
-    /**
-     * Get the node's representation as DokuWiki Syntax
-     *
-     * @return string
-     */
-    public function toSyntax()
-    {
-        /** @var syntax_plugin_gallery $syntax */
-        $syntax = plugin_load('syntax', 'gallery');
-        $defaults = $syntax->getDataFromParams($syntax->getConf('options'));
-        /** @var action_plugin_gallery_prosemirror $action */
-        $action = plugin_load('action', 'gallery_prosemirror');
-        $defaults = $action->cleanAttributes($defaults);
-        $query = [];
-        $attrs = $this->data['attrs'];
-        if ($attrs['thumbnailsize'] !== $defaults['thumbnailsize']) {
-            $query[] = $attrs['thumbnailsize'];
-        }
-        if ($attrs['imagesize'] !== $defaults['imagesize']) {
-            $query[] = $attrs['imagesize'];
-        }
-
-        $query[] = $this->extractFlagParam('showname', $attrs, $defaults);
-        $query[] = $this->extractFlagParam('showtitle', $attrs, $defaults);
-        $query[] = $this->extractFlagParam('cache', $attrs, $defaults);
-        $query[] = $this->extractFlagParam('crop', $attrs, $defaults);
-        $query[] = $this->extractFlagParam('direct', $attrs, $defaults);
-        $query[] = $this->extractFlagParam('lightbox', $attrs, $defaults);
-        $query[] = $this->extractFlagParam('reverse', $attrs, $defaults);
-        $query[] = $this->extractFlagParam('recursive', $attrs, $defaults);
-        $query = array_filter($query);
-
-        if ((int)$attrs['cols'] !== (int)$defaults['cols']) {
-            $query[] = $attrs['cols'];
-        }
-        if ((int)$attrs['limit'] !== (int)$defaults['limit']) {
-            $query[] = '=' . $attrs['limit'];
-        }
-        if ((int)$attrs['offset'] !== (int)$defaults['offset']) {
-            $query[] = '+' . $attrs['offset'];
-        }
-        if ((int)$attrs['paginate'] !== (int)$defaults['paginate']) {
-            $query[] = '~' . $attrs['paginate'];
-        }
-        if ((int)$attrs['sort'] !== (int)$defaults['sort']) {
-            $query[] = $attrs['sort'];
-        }
-        if ($attrs['filter'] && strpos($attrs['filter'], '*') !== false) {
-            $query[] = $attrs['filter'];
-        }
-        $alignLeft = $attrs['align'] === 'left' ? ' ' : '';
-        $alignRight = $attrs['align'] === 'right' ? ' ' : '';
-        $result = '{{gallery>' . $alignRight . $attrs['namespace'];
-        if (!empty($query)) {
-            $result .= '?' . implode('&', $query);
-        }
-        $result .= $alignLeft . '}}';
-        return $result;
-    }
-
-    /**
-     * Get syntax option-string if the option is different from the default
-     *
-     * @param string $paramName The name of the parameter
-     * @param array  $data      The node's attributes from the editor
-     * @param array  $defaults  The default options
-     *
-     * @return null|string Either the string to toggle the option away from the default or null
-     */
-    protected function extractFlagParam($paramName, $data, $defaults)
-    {
-        if ((bool)$data[$paramName] !== (bool)$defaults[$paramName]) {
-            return ($defaults[$paramName] ? 'no' : '') . $paramName;
-        }
-        return null;
-    }
-}
