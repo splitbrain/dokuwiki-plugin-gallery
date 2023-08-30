0b7f9809698cb1f9a4a2442aef193316efda5dac
Sani <sinerioul@gmail.com>
2023-08-30 14:55:05 +0200

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,36 +1,38 @@
 <?php
 
-$lang['pages'] = 'Gallery Pages:';
-$lang['js']['addgal'] = 'Add namespace as gallery';
-$lang['nothingfound'] = 'No images found.';
-$lang['fail'] = 'Failed to load gallery';
-
-
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ */
+$lang['pages']                 = 'Gallery Pages:';
+$lang['js']['addgal']          = 'Add namespace as gallery';
 $lang['js']['label_toolbar_button'] = 'Insert Gallery';
-$lang['js']['title_dialog'] = 'Gallery Plugin';
+$lang['js']['title_dialog']    = 'Gallery Plugin';
 $lang['js']['label_namespace'] = 'Namespace or RSS feed';
 $lang['js']['label_thumbnailsize'] = 'Sets the size for thumbnails';
 $lang['js']['pattern_hint_thumbnailsize'] = '<number><lowercase x><number>';
 $lang['js']['label_imagesize'] = 'Sets the size for the linked images in direct mode.';
 $lang['js']['pattern_hint_imagesize'] = '<number><uppercase X><number>';
-$lang['js']['label_cache'] = 'Cache the thumbnails';
-$lang['js']['label_filter'] = 'Filter (must contain a "*" character)';
-$lang['js']['label_showname'] = 'Show filename below thumbnails';
+$lang['js']['label_cache']     = 'Cache the thumbnails';
+$lang['js']['label_filter']    = 'Filter (must contain a "*" character)';
+$lang['js']['label_showname']  = 'Show filename below thumbnails';
 $lang['js']['label_showtitle'] = 'Show the EXIF tag Headline as title below thumbnails';
-$lang['js']['label_crop'] = 'Make thumbnails the exact given thumbnail size big, cropping when needed';
-$lang['js']['label_direct'] = 'Link thumbnails with the bigger sized images not with their detail page';
-$lang['js']['label_lightbox'] = 'Show images in a fancy JavaScript modal';
-$lang['js']['label_reverse'] = 'Reverse the order of the displayed images';
+$lang['js']['label_crop']      = 'Make thumbnails the exact given thumbnail size big, cropping when needed';
+$lang['js']['label_direct']    = 'Link thumbnails with the bigger sized images not with their detail page';
+$lang['js']['label_lightbox']  = 'Show images in a fancy JavaScript modal';
+$lang['js']['label_reverse']   = 'Reverse the order of the displayed images';
 $lang['js']['label_recursive'] = 'Find images in the given namespace and all sub namespaces';
 $lang['js']['label_align_left'] = 'align left';
 $lang['js']['label_align_center'] = 'align center';
 $lang['js']['label_align_right'] = 'align right';
-$lang['js']['label_cols'] = 'The number images per row in the gallery table';
-$lang['js']['label_limit'] = 'Limits the output to the given number of images. 0 means all';
-$lang['js']['label_offset'] = 'Skip the first number of images. Useful with the option above';
-$lang['js']['label_paginate'] = 'Add a pagination for the thumbnails displaying the number of given thumbnails per page. 0 disables pagination';
+$lang['js']['label_cols']      = 'The number images per row in the gallery table';
+$lang['js']['label_limit']     = 'Limits the output to the given number of images. 0 means all';
+$lang['js']['label_offset']    = 'Skip the first number of images. Useful with the option above';
+$lang['js']['label_paginate']  = 'Add a pagination for the thumbnails displaying the number of given thumbnails per page. 0 disables pagination';
 $lang['js']['label_sort_file'] = 'Sort the images by filename';
 $lang['js']['label_sort_random'] = 'Sort images randomly. You might want to disable the cache as well';
-$lang['js']['label_sort_mod'] = 'Sort images by file modification date';
+$lang['js']['label_sort_mod']  = 'Sort images by file modification date';
 $lang['js']['label_sort_exif_date'] = 'Sort images by EXIF date';
 $lang['js']['label_sort_exif_title'] = 'Sort images by EXIF title';
+$lang['nothingfound']          = 'No images found.';
+$lang['fail']                  = 'Failed to load gallery';
