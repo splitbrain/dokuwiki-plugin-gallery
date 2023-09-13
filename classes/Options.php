7d44814ddb26da622bf4bcbbdaf537be26a3339a
Andreas Gohr <andi@splitbrain.org>
2023-09-13 14:32:03 +0200

    fix PHP 7.2 compatibility
    For now we want to keep compatibility with 7.2 so it stays compatible
    with all stable setups

--- classes/Options.php
+++ classes/Options.php
@@ -16,27 +16,27 @@ class Options
     public const ALIGN_CENTER = 3;
 
     // defaults
-    public string $galleryID = '';
-    public int $thumbnailWidth = 120;
-    public int $thumbnailHeight = 120;
-    public int $lightboxWidth = 1600;
-    public int $lightboxHeight = 1200;
-    public int $columns = 0;
-    public string $filter = '';
-    public bool $lightbox = false;
-    public bool $direct = false;
-    public bool $showcaption = false;
-    public bool $showname = false;
-    public bool $showtitle = false;
-    public bool $reverse = false;
-    public bool $cache = false;
-    public bool $crop = false;
-    public bool $recursive = false;
+    public $galleryID = '';
+    public $thumbnailWidth = 120;
+    public $thumbnailHeight = 120;
+    public $lightboxWidth = 1600;
+    public $lightboxHeight = 1200;
+    public $columns = 0;
+    public $filter = '';
+    public $lightbox = false;
+    public $direct = false;
+    public $showcaption = false;
+    public $showname = false;
+    public $showtitle = false;
+    public $reverse = false;
+    public $cache = false;
+    public $crop = false;
+    public $recursive = false;
     public $sort = self::SORT_FILE;
-    public int $limit = 0;
-    public int $offset = 0;
-    public int $paginate = 0;
-    public int $align = self::ALIGN_FULL;
+    public $limit = 0;
+    public $offset = 0;
+    public $paginate = 0;
+    public $align = self::ALIGN_FULL;
 
     /**
      * Options constructor.
