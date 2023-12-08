f9583075464585008fcc9703229629278790d89a
Yaroslav <zevs91@gmail.com>
2023-12-08 09:35:29 +0100

    translation update

---
+++ lang/uk/lang.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @license    GPL 2 (https://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Yaroslav <zevs91@gmail.com>
+ */
+$lang['pages']                 = 'Сторінки галереї:';
+$lang['js']['title_dialog']    = 'Плагін галереї';
+$lang['js']['label_align_left'] = 'вирівняти зліва';
+$lang['js']['label_align_center'] = 'вирівняти по центру';
+$lang['js']['label_align_right'] = 'вирівняти справа';
+$lang['js']['label_cols']      = 'Кількість зображень на рядок у таблиці галереї';
+$lang['js']['label_offset']    = 'Пропустити першу кількість зображень. Корисно з опцією вище';
+$lang['js']['label_sort_file'] = 'Сортувати за ім\'ям файлу';
+$lang['js']['label_sort_mod']  = 'Сортувати за датою змінювання';
+$lang['nothingfound']          = 'Зображення не знайдено.';
+$lang['fail']                  = 'Помилка завантаження галереї';
