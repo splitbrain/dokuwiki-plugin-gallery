193e35afeaee0712039b494486ad5729bb10e884
Martin Růžička <martinr@post.cz>
2024-07-21 16:00:41 +0200

    translation update

--- lang/cs/lang.php
+++ lang/cs/lang.php
@@ -3,9 +3,37 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Martin Růžička <martinr@post.cz>
  * @author Petr Kajzar <petr.kajzar@lf1.cuni.cz>
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  */
 $lang['pages']                 = 'Stránky galerie:';
 $lang['js']['addgal']          = 'Přidat jmenný prostor jako galerii';
+$lang['js']['label_toolbar_button'] = 'Vložit galerii';
+$lang['js']['title_dialog']    = 'Modul plug-in Galerie';
+$lang['js']['label_namespace'] = 'Jmenný prostor nebo RSS kanál';
+$lang['js']['label_thumbnailsize'] = 'Nastavit velikost miniatur';
+$lang['js']['label_imagesize'] = 'Nastavit velikost propojených obrázků v přímém režimu';
+$lang['js']['label_cache']     = 'Uložit miniatury do mezipaměti';
+$lang['js']['label_filter']    = 'Filtr (musí obsahovat znak *)';
+$lang['js']['label_showname']  = 'Zobrazit název souboru pod miniaturami';
+$lang['js']['label_showtitle'] = 'Zobrazit EXIF ​​tag Headline jako nadpis pod miniaturami';
+$lang['js']['label_crop']      = 'Vytvořit miniatury přesně dané velikosti a v případě potřeby je oříznout';
+$lang['js']['label_direct']    = 'Propojit miniatury s obrázky větších rozměrů, nikoli se stránkou s podrobnostmi';
+$lang['js']['label_lightbox']  = 'Zobrazit obrázky v efektním modálním okně JavaScriptu';
+$lang['js']['label_reverse']   = 'Obrátit pořadí zobrazených snímků';
+$lang['js']['label_recursive'] = 'Vyhledat obrázky v daném jmenném prostoru a všech podřízených jmenných prostorech';
+$lang['js']['label_align_left'] = 'Zarovnat vlevo';
+$lang['js']['label_align_center'] = 'Zarovnat na střed';
+$lang['js']['label_align_right'] = 'Zarovnat vpravo';
+$lang['js']['label_cols']      = 'Počet obrázků na řádek v tabulce galerie';
+$lang['js']['label_limit']     = 'Omezit výstup na zadaný počet obrázků. 0 znamená všechny';
+$lang['js']['label_offset']    = 'Přeskočit první počet obrázků. Užitečné s možností výše';
+$lang['js']['label_paginate']  = 'Přidat stránkování pro miniatury zobrazující počet daných miniatur na stránku. 0 zakáže stránkování';
+$lang['js']['label_sort_file'] = 'Seřadit obrázky podle názvu souboru';
+$lang['js']['label_sort_random'] = 'Seřadit obrázky náhodně. Možná budete chtít také zakázat mezipaměť';
+$lang['js']['label_sort_mod']  = 'Seřadit obrázky podle data změny souboru';
+$lang['js']['label_sort_exif_date'] = 'Seřadit obrázky podle data EXIF';
+$lang['js']['label_sort_exif_title'] = 'Seřadit obrázky podle názvu EXIF';
 $lang['nothingfound']          = 'Nenalezeny žádné obrázky.';
+$lang['fail']                  = 'Načtení galerie se nezdařilo';
