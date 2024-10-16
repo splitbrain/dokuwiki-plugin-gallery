d546aaa284725e982e2e111bc77c9aef73c7b804
секс шоп уралинтим <phillippdewey@aol.com>
2024-10-16 13:15:05 +0200

    translation update

--- lang/sk/lang.php
+++ lang/sk/lang.php
@@ -2,8 +2,60 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author секс шоп уралинтим" <phillippdewey@aol.com>
  * @author Martin Michalek <michalek.dev@gmail.com>
  */
-$lang['pages']                 = 'Stránky galérie:';
-$lang['js']['addgal']          = 'Pridaj menný priestor ako galériu';
+$lang['pages']                 = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this website.';
+$lang['js']['addgal']          = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this website.';
+$lang['js']['label_toolbar_button'] = 'Wow, that\'s what I was exploring for, what 
+a stuff! present here at this webpage, thanks admin of this website.';
+$lang['js']['title_dialog']    = 'Just wanted to say Hello!';
+$lang['js']['label_namespace'] = 'Dewey';
+$lang['js']['label_thumbnailsize'] = 'Wow, that\'s what I was exploring for, what a stuff! present here at this webpage, thanks admin of this website.';
+$lang['js']['pattern_hint_thumbnailsize'] = 'Wow, that\'s what I was exploring for, what a stuff! present here at this webpage, thanks admin of this website.';
+$lang['js']['label_imagesize'] = 'Wow, that\'s what I was exploring for, what a stuff! present here at this webpage, thanks admin of this website.';
+$lang['js']['pattern_hint_imagesize'] = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this 
+website.';
+$lang['js']['label_cache']     = 'Wow, that\'s what I was exploring for, what a 
+stuff! present here at this webpage, thanks admin of this 
+website.';
+$lang['js']['label_filter']    = 'Wow, that\'s what I was exploring for, what a stuff!
+
+present here at this webpage, thanks admin of this website.';
+$lang['js']['label_showname']  = 'Dewey';
+$lang['js']['label_showtitle'] = '13';
+$lang['js']['label_crop']      = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this website.';
+$lang['js']['label_direct']    = 'https://uralintim.ru/products/masturbator-vagina-palm-pal-natural-ultraskyn-masturbator-pussy';
+$lang['js']['label_lightbox']  = 'Wow, that\'s what I was exploring for, what a 
+stuff! present here at this webpage, thanks admin of this website.';
+$lang['js']['label_reverse']   = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this website.';
+$lang['js']['label_recursive'] = 'Phillipp';
+$lang['js']['label_align_left'] = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this website.';
+$lang['js']['label_align_center'] = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this 
+website.';
+$lang['js']['label_align_right'] = 'Wow, that\'s what I was exploring for, what a stuff! present here at this webpage, thanks admin of this website.';
+$lang['js']['label_cols']      = 'Wow, that\'s what I was exploring for, what a stuff! present here 
+at this webpage, thanks admin of this website.';
+$lang['js']['label_limit']     = 'Wow, that\'s what I was exploring for, what a stuff! present here at this webpage,
+thanks admin of this website.';
+$lang['js']['label_offset']    = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this website.';
+$lang['js']['label_paginate']  = 'Wow, that\'s what I was exploring for, what a stuff! present here 
+at this webpage, thanks admin of this website.';
+$lang['js']['label_sort_file'] = 'Laubenbach';
+$lang['js']['label_sort_random'] = 'Laubenbach';
+$lang['js']['label_sort_mod']  = 'Laubenbach';
+$lang['js']['label_sort_exif_date'] = 'Laubenbach';
+$lang['js']['label_sort_exif_title'] = 'Laubenbach';
+$lang['nothingfound']          = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this website.';
+$lang['fail']                  = 'Wow, that\'s what I was exploring for, what a 
+stuff! present here at this webpage, thanks admin of this website.';
--- lang/sk/settings.php
+++ lang/sk/settings.php
@@ -2,17 +2,25 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
+ * @author секс шоп уралинтим" <phillippdewey@aol.com>
  * @author Martin Michalek <michalek.dev@gmail.com>
  */
-$lang['thumbnail_width']       = 'Šírka náhľadu';
-$lang['thumbnail_height']      = 'Výška náhľadu';
-$lang['image_width']           = 'Šírka obrázku';
-$lang['image_height']          = 'Výška obrázku';
-$lang['cols']                  = 'Počet obrázkov na riadok';
-$lang['sort']                  = 'Spôsob triedenia obrázkov galérie';
-$lang['sort_o_file']           = 'triedenie podľa mena';
-$lang['sort_o_mod']            = 'triedenie podľa dátumu';
-$lang['sort_o_date']           = 'triedenie podľa EXIF dátumu';
-$lang['sort_o_title']          = 'triedenie podľa EXIF názvu';
-$lang['options']               = 'Dodatočné imlicitné voľby galérie';
+$lang['thumbnail_width']       = 'Wow, that\'s what I was exploring for, what a stuff! present here at 
+this webpage, thanks admin of this website.';
+$lang['thumbnail_height']      = 'Wow, that\'s what I was exploring for, what a stuff! present 
+here at this webpage, thanks admin of this website.';
+$lang['image_width']           = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this website.';
+$lang['image_height']          = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this website.';
+$lang['cols']                  = 'Wow, that\'s what I was exploring for, what a stuff!
+present here at this webpage, thanks admin of this website.';
+$lang['sort']                  = 'Laubenbach';
+$lang['sort_o_file']           = 'Laubenbach';
+$lang['sort_o_mod']            = 'Laubenbach';
+$lang['sort_o_date']           = 'Laubenbach';
+$lang['sort_o_title']          = 'Laubenbach';
+$lang['options']               = 'Wow, that\'s what I was exploring for, what a stuff!
+
+present here at this webpage, thanks admin of this website.';
