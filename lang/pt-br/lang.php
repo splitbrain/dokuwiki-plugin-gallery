69d9a9baf425ca04cc8efba55672b5b0be580b82
Jeann <wjeann@gmail.com>
2025-01-18 16:10:45 +0100

    translation update

--- lang/pt-br/lang.php
+++ lang/pt-br/lang.php
@@ -3,9 +3,15 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Jeann <wjeann@gmail.com>
  * @author Frederico Gonçalves Guimarães <frederico@teia.bio.br>
  * @author Juliano Marconi Lanigra <juliano.marconi@gmail.com>
  */
 $lang['pages']                 = 'Páginas da galeria:';
 $lang['js']['addgal']          = 'Adicionar domínio como galeria';
+$lang['js']['label_sort_random'] = 'Organizar imagens randomicamente. Você pode querer desabilitar o cache também';
+$lang['js']['label_sort_mod']  = 'Organizar imagens por data de modificação';
+$lang['js']['label_sort_exif_date'] = 'Organizar imagens por data EXIF';
+$lang['js']['label_sort_exif_title'] = 'Organizar imagens por título EXIF ';
 $lang['nothingfound']          = 'Não foi encontrada nenhuma imagem.';
+$lang['fail']                  = 'Falha ao carregar a galeria';
